a8feda2ee23868bfeba5b4893de22ccd7757e080
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2019-06-26 20:08:23 +0300

    forgot this whoops
    Co-Authored-By: Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>

--- src/commands/Template Management/removetemplate.ts
+++ src/commands/Template Management/removetemplate.ts
@@ -16,7 +16,7 @@ export default class extends Command {
 		const templates = this.client.settings.get(ClientSettings.GuildTemplates) as ClientSettings.GuildTemplates;
 		const relevantTemplate = templates.find(t => t.name.toLowerCase() === templateName.toLowerCase());
 		if(!relevantTemplate) return message.channel.send("I couldn't find that template! Please check your spelling if you are ***sure*** that template exists") as any;
-		const templateGuild = this.client.guilds.get(relevantTemplates[0].id);
+		const templateGuild = this.client.guilds.get(relevantTemplates.id);
 		const member = await templateGuild.members.fetch()
 		if (!member) return message.sendMessage(‘Are you sure you are a member on this template server? In order to remove a template, you have to have Manager Server permissions on the template server.’)
 		if(!member.hasPermission(Permissions.FLAGS.MANAGE_GUILD))	return message.sendMessage("You don't seem to have enough permissions to do that! Only members of the template server with the `MANAGE SERVER` permission are allowed to delete templates");
