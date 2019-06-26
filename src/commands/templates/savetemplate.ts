97cf164dbdf8a2929688e429aaaff7a067f64a73
VoidCodes <harodtai321@gmail.com>
2019-06-26 09:01:40 +0800

    -

--- src/commands/templates/savetemplate.ts
+++ src/commands/templates/savetemplate.ts
@@ -11,17 +11,23 @@ import { ClientSettings } from '../../lib/types/settings/ClientSettings';
 export default class extends Command {
     constructor(store: CommandStore, file: string[], directory: string) {
         super(store, file, directory, {
-            runIn: ['text'],
+      runIn: ['text'],
 			aliases: ['savetemp'],
 			permissionLevel: 4,
 			description: 'Saves the template',
 			usage: '<templateName:string>'
         })
     }
-  //KlasaGuild isnt reading from Iimports
-    async run(message: KlasaMessage, [guild: KlasaGuild]){
+  
+    async run(message: KlasaMessage, [templateName]: [string]){
 
-      await this.client.settings.update(ClientSettings.GuildTemplates, { name: 'templateName', id: message.guild.id })
+      this.customizeResponse('templateName', 'Saving template...')
+
+      const template = this.client.settings.get(ClientSettings.GuildTemplates) as ClientSettings.GuildTemplates
+
+      if(template.id) return message.send('This template exists already')
+
+      await this.client.settings.update(ClientSettings.GuildTemplates, { name: templateName, id: message.guild.id })
 
        message.send('Template saved')
     }
