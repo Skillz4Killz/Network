187e68548c5f0141d303fecb7ab97f9d89042209
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2019-06-26 20:05:06 +0300

    requested change 2
    Co-Authored-By: Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>

--- src/commands/Template Management/removetemplate.ts
+++ src/commands/Template Management/removetemplate.ts
@@ -15,7 +15,7 @@ export default class extends Command {
     async run(message: KlasaMessage, [templateName]: [string]): Promise<KlasaMessage> {
 		const templates = this.client.settings.get(ClientSettings.GuildTemplates) as ClientSettings.GuildTemplates;
 		const relevantTemplate = templates.find(t => t.name.toLowerCase() === templateName.toLowerCase());
-		if(!relevantTemplates) return message.channel.send("I couldn't find that template! Please check your spelling if you are ***sure*** that template exists") as any;
+		if(!relevantTemplate) return message.channel.send("I couldn't find that template! Please check your spelling if you are ***sure*** that template exists") as any;
 		const templateGuild = this.client.guilds.get(relevantTemplates[0].id);
 		await templateGuild.members.fetch()
 		if(!templateGuild.members.get(message.author.id).hasPermission(Permissions.FLAGS.MANAGE_GUILD))	return message.channel.send("You don't seem to have enough permissions to do that! Only members of the template server with the `MANAGE SERVER` permission are allowed to delete templates") as any;
