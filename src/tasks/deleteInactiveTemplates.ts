2f5bd5fe757e847a2b9ef74e81f1e4f90432a244
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2019-06-27 12:28:42 +0300

    Requested changes

---
+++ src/tasks/deleteInactiveTemplates.ts
@@ -0,0 +1,29 @@
+import { Task } from "../imports";
+import { ClientSettings } from "../lib/types/settings/ClientSettings";
+
+export default class extends Task {
+
+    async run() {
+
+        // Get our templates
+        const templates = await this.client.settings.get(ClientSettings.GuildTemplates) as ClientSettings.GuildTemplates;
+        const inactive = [];
+
+        /* Run over every template and check if the guild it refers to still exists in our cache. 
+         If not, add the id to the `inactive` array */
+        for (const template of templates) {
+            const guild = this.client.guilds.get(template.id);
+            if (!guild) inactive.push(template.id);
+        }
+
+        // Filter out all the inactive guilds
+        const newTemplates = templates.filter(template => !inactive.includes(template.id));
+
+        // Send the new array to SG
+        await this.client.settings.update(ClientSettings.GuildTemplates, newTemplates, { arrayAction: "overwrite", throwOnError: true })
+    }
+
+    async init(): Promise<void> {
+        this.client.schedule.create('deleteInactiveTemplates', '@daily', { id: "dia", catchUp: false});
+    }
+};
