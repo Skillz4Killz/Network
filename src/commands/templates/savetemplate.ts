23f0264e6e829b01b4ee4d8f38612c5cbe198b3c
Skillz <gankstarsleague@gmail.com>
2019-06-27 15:41:18 -0400

    fixes on savetemplate

--- src/commands/templates/savetemplate.ts
+++ src/commands/templates/savetemplate.ts
@@ -1,29 +1,30 @@
-import { Command, CommandStore, KlasaMessage } from '../../imports'
+import { Command, CommandStore, KlasaMessage } from '../../imports';
 import { ClientSettings } from '../../lib/types/settings/ClientSettings';
 
 export default class extends Command {
-    constructor(store: CommandStore, file: string[], directory: string) {
-      super(store, file, directory, {
-      runIn: ['text'],
+
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			runIn: ['text'],
 			aliases: ['savetemp'],
 			permissionLevel: 7,
 			description: 'Saves this server as a template, so you can create another server with the same format.',
 			usage: '<templateName:string>'
-      })
+		});
 
-      this.customizeResponse('templateName', 'You did not provide a template name. Please try the command again and give a name you wish to save the template as.')
-    }
-  
-    async run(message: KlasaMessage, [templateName]: [string]){
+		this.customizeResponse('templateName', 'You did not provide a template name. Please try the command again and give a name you wish to save the template as.');
+	}
 
-      const templates = this.client.settings.get(ClientSettings.GuildTemplates) as ClientSettings.GuildTemplates
+	public async run(message: KlasaMessage, [templateName]: [string]) {
+		const templates = this.client.settings.get(ClientSettings.GuildTemplates) as ClientSettings.GuildTemplates;
 
-      const relevanttemp = templates.find(temp => temp.id === message.guild.id)
+		const template = templates.find(temp => temp.id === message.guild.id);
 
-      if(relevanttemp) return message.send(`This template already exists under the name of ${relevanttemp.name}`)
+		if (template) return message.send(`This template already exists under the name of ${template.name}`);
 
-      await this.client.settings.update(ClientSettings.GuildTemplates, { name: templateName, id: message.guild.id })
+		await this.client.settings.update(ClientSettings.GuildTemplates, { name: templateName, id: message.guild.id });
 
-      return message.send(`The template has been saved with the name **${templateName}**. You can now add the bot to new server and type **.createserver ${templateName}** to create a copy of the server.`)
-    }
+		return message.send(`The template has been saved with the name **${templateName}**. You can now add the bot to new server and type **.createserver ${templateName}** to create a copy of the server.`);
+	}
+
 }
