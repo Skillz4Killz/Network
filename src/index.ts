43c1cf456dd0c541ab045ef8d62fdd7ba2286a46
Skillz <gankstarsleague@gmail.com>
2019-06-27 12:05:15 -0400

    proper alphabetical order

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@ new KlasaClient({
 		'default': 'postgresql',
 		'postgresql': configs.postgresql
 	},
-	typing: true,
-	readyMessage: client => `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`
+	readyMessage: client => `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`,
+	typing: true
 }).login(configs.botToken);
