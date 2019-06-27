ff375cdc5a6e91c8c0bf2fd171f835c8d90a8f9d
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2019-06-27 22:33:41 +0300

    requested changes

--- src/commands/Social Network/follow.ts
+++ src/commands/Social Network/follow.ts
@@ -5,8 +5,7 @@ export default class extends Command {
 
 	public constructor(store: CommandStore, file: string[], directory: string) {
 		super(store, file, directory, {
-			name: 'follow',
-			description: 'Follows/Unfollows a user',
+			description: 'Follows/Unfollows a user, thus mkaing his posts appear in your feed',
 			usage: '<user:user>'
 		});
 	}
@@ -15,22 +14,19 @@ export default class extends Command {
 
 		try {
 			// Get an array of users that our user is following
-			const following = message.author.settings.get('following') as KlasaUser[];
+			const following = message.author.settings.get(UserSettings.Following) as UserSettings.Following;
 
 			// Check if the users already follows the user specified in the command
-			const isAlreadyFollowing = following.some(followed => !(followed.id === user.id));
+			const isAlreadyFollowing = following.includes(user.id);
 
-			/* Tell SG we have this user, and we need to add/remove it. In case it exists, it gets removed. Elsewise,
-			*  the user is simply pushed into the array
-			*/
-			await user.settings.update(UserSettings.Following, user, { throwOnError: true });
+			// Toggle the user in SG
+			await user.settings.update(UserSettings.Following, user.id, { throwOnError: true });
 
-			message.sendMessage(`Successfully ${isAlreadyFollowing ? 'un' : ''}followed ${user.username}`);
-
+			return message.sendMessage(`Successfully ${isAlreadyFollowing ? 'un' : ''}followed ${user.username}`);
 		} catch (e) {
 			// Classic error handling
-			message.sendMessage('Gadzooks! An error has occured. My developers have been informed about it.');
 			this.client.emit('error', e);
+			return message.sendMessage('Gadzooks! An error has occured. My developers have been informed about it.');
 		}
 
 	}
