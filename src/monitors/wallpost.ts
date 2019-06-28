c56a474992a68006d879de917696debbd273232d
Skillz <gankstarsleague@gmail.com>
2019-06-28 11:22:23 -0400

    adds feed support

--- src/monitors/wallpost.ts
+++ src/monitors/wallpost.ts
@@ -1,6 +1,7 @@
 import { KlasaMessage, Monitor, MonitorStore } from 'klasa';
 import { GuildSettings } from '../lib/types/settings/GuildSettings';
-import { TextChannel, MessageEmbed, Message } from 'discord.js';
+import { TextChannel, MessageEmbed, Message, User } from 'discord.js';
+import { UserSettings } from '../lib/types/settings/UserSettings';
 
 export default class extends Monitor {
 
@@ -13,6 +14,9 @@ export default class extends Monitor {
 	public async run(message: KlasaMessage) {
 		// If the user doesnt have permissions to add reactions
 		if (!(message.channel as TextChannel).permissionsFor(message.guild.me).has('ADD_REACTIONS')) return null;
+
+		// Only owners can post
+		if (message.guild.ownerID !== message.author.id) return null;
 
 		// If the guild doesnt have a wall channel or if this is NOT the wall channel cancel it
 		const [wallChannelID, photosChannelID] = message.guild.settings.pluck(GuildSettings.Channels.WallID, GuildSettings.Channels.PhotosID);
@@ -44,7 +48,24 @@ export default class extends Monitor {
 			const photosChannel = message.guild.channels.get(photosChannelID) as TextChannel;
 			if (!photosChannel) return posted;
 
-			return photosChannel.send(embed);
+			await photosChannel.send(embed);
+
+			// Now we have to repost this in every followers feed
+			for (const user of this.client.users.values()) {
+				const [followers, serverID] = user.settings.pluck(UserSettings.Following, UserSettings.Profile.ServerID);
+				// If this user is not following then skip
+				if (!followers.includes(message.author.id)) continue;
+
+				// This user is following so get the profile server for this user
+				const guild = this.client.guilds.get(serverID);
+				if (!guild) return null;
+
+				// Get the channel from the settings
+				const [feedChannel] = await guild.settings.resolve(GuildSettings.Channels.FeedID);
+				if (!feedChannel) return null;
+
+				return feedChannel.send(embed);
+			}
 		} catch (error) {
 			// Silently error cause monitors are way too risky to send messages
 			return this.client.emit('error', error);
