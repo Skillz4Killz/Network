b836768d902c4d308f2c18088807268cbddd69a4
Skillz <gankstarsleague@gmail.com>
2019-06-28 10:59:59 -0400

    adds support for photos album

--- src/monitors/wallpost.ts
+++ src/monitors/wallpost.ts
@@ -15,14 +15,16 @@ export default class extends Monitor {
 		if (!(message.channel as TextChannel).permissionsFor(message.guild.me).has('ADD_REACTIONS')) return null;
 
 		// If the guild doesnt have a wall channel or if this is NOT the wall channel cancel it
-		const wallChannelID = message.guild.settings.get(GuildSettings.Channels.WallID) as GuildSettings.Channels.TextChannelID;
+		const [wallChannelID, photosChannelID] = message.guild.settings.pluck(GuildSettings.Channels.WallID, GuildSettings.Channels.PhotosID);
 		if (!wallChannelID || (wallChannelID !== message.channel.id)) return null;
+
+		const imageURL = message.attachments.size ? message.attachments.first().url : null;
 
 		const embed = new MessageEmbed()
 			.setAuthor(message.author.tag, message.author.displayAvatarURL())
 			.setColor('RANDOM')
 			.setDescription(message.content)
-			.setImage(message.attachments.size ? message.attachments.first().url : null)
+			.setImage(imageURL)
 			.setTimestamp()
 			.setFooter(message.author.id);
 
@@ -35,7 +37,14 @@ export default class extends Monitor {
 			// Delete the original message the author posted to keep channel clean
 			if (message.deletable && !message.deleted) await message.delete();
 
-			return posted;
+			// If there was no image then simple cancel out
+			if (!imageURL || !photosChannelID) return posted;
+
+			// If an image was attached post the image in #photos
+			const photosChannel = message.guild.channels.get(photosChannelID) as TextChannel;
+			if (!photosChannel) return posted;
+
+			return photosChannel.send(embed);
 		} catch (error) {
 			// Silently error cause monitors are way too risky to send messages
 			return this.client.emit('error', error);
