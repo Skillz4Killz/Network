e1410c2837b3e33be6c30e6b577f75bdbb57b2cb
Skillz <gankstarsleague@gmail.com>
2019-06-28 10:59:37 -0400

    fix info command string

--- src/languages/en-US.ts
+++ src/languages/en-US.ts
@@ -2,6 +2,29 @@ import { Language } from '../imports';
 
 export default class extends Language {
 
-	language = {};
+	public language = {
+		COMMAND_INFO: [
+			"Network Bot is a bot built for the Discord Hackathon June 2019. It's built with Klasa framework built on top of the Discord.js library.",
+			'The bot adds some key functionalities to discord:',
+			'',
+			'✅ True social network',
+			"• 💬 It's time to ditch Twitter and Facebook.",
+			"    • All-in-one voice and text chat social network that's free, secure, and works on both your desktop and phone.",
+			'    • Stop risking your private info with Facebook and hassling with Twitter. Simplify your life.',
+			'    • Create your social network with **.createnetwork**',
+			'    • Make posts, like posts with ❤ reaction, Repost on your #wall with 🔁 reaction, Follow users with ➕ reaction.',
+			'',
+			'✅ Server template networks',
+			'• 🌐 Create a fresh copy of a server based on a template server.',
+			'    • Use one of the predefined templates. (**.createschool**)',
+			'    • Create your own custom templates. (**.savetemplate**, **.removetemplate**, **.createserver GuildID**)',
+			'',
+			'✅ Meet similar new people',
+			'• 🌐 Create a fresh copy of a server based on a template server.',
+			'',
+			'We hope Discord will love the idea enough to consider us a winner and even add the feature to Discord directly.',
+			'Discord is Love! Discord is Life!'
+		]
+	};
 
 }
