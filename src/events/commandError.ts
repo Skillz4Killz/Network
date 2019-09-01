66814c25decd3d11e1a084143ad455fe88d48204
Skillz <gankstarsleague@gmail.com>
2019-09-01 10:08:07 -0400

    handle piece errors inside wtf.ts instead

--- src/events/commandError.ts
+++ src/events/commandError.ts
@@ -5,15 +5,18 @@ export default class extends Event {
 
 	public run(message: KlasaMessage, command: Command, _params: any[], error) {
 		if (error instanceof Error) this.client.emit('wtf', `[COMMAND] ${command.path}\n${error.stack || error}`);
-		if (error.message) message.sendCode('JSON', error.message).catch(err => this.client.emit('wtf', err));
-		else message.sendMessage(error).catch(err => this.client.emit('wtf', err));
+		if (error.message) {
+			message.sendCode('JSON', error.message).catch(err => this.client.emit('wtf', err));
+		} else {
+			message.sendMessage(error).catch(err => this.client.emit('wtf', err));
+			// Emit an error to a discord channel
+			const errorLogChannel = this.client.channels.get(configs.errorLogChannelID) as TextChannel;
+			// If the channel wasnt found or missing permission to post in it
+			if (!errorLogChannel || !errorLogChannel.postable) return null;
 
-		// Emit an error to a discord channel
-		const errorLogChannel = this.client.channels.get(configs.errorLogChannelID) as TextChannel;
-		// If the channel wasnt found or missing permission to post in it
-		if (!errorLogChannel || !errorLogChannel.postable) return null;
+			return errorLogChannel.sendCode('ts', error.message || error);
+		}
 
-		return errorLogChannel.sendCode('ts', error.message || error);
 	}
 
 }
--- src/events/wtf.ts
+++ src/events/wtf.ts
@@ -3,20 +3,19 @@ import configs from '../../configs';
 
 export default class extends Event {
 
-	public run(event, _args, error) {
-		const errorMessage = `[EVENT] ${event.path}\n${error
-			? error.stack
-				? error.stack
-				: error
-			: 'Unknown error'}`;
-		this.client.emit('wtf', errorMessage);
+	public run(failure) {
+		this.client.console.wtf(failure);
 
 		// Emit an error to a discord channel
 		const errorLogChannel = this.client.channels.get(configs.errorLogChannelID) as TextChannel;
 		// If the channel wasnt found or missing permission to post in it
 		if (!errorLogChannel || !errorLogChannel.postable) return null;
 
-		return errorLogChannel.sendCode('ts', errorMessage);
+		return errorLogChannel.sendCode('ts', failure);
 	}
 
-}
+	public async init() {
+		if (!this.client.options.consoleEvents.wtf) this.disable();
+	}
+
+};
