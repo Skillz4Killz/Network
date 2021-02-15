cf62dd181d87f7da48a5d606a71f36673e6f2feb
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-15 22:48:38 +0100

    Update cache.ts

--- cache.ts
+++ cache.ts
@@ -1,13 +1,20 @@
 import { configs } from "./configs.ts";
-import { Collection } from "./deps.ts";
+import { Collection, Guild, Message } from "./deps.ts";
+import { MessageCollector, ReactionCollector } from "./src/types/collectors.ts";
+import { Argument, Command, PermissionLevels } from "./src/types/commands.ts";
+import { Constants } from "./src/types/constants.ts";
+import { CustomEvents } from "./src/types/events.ts";
+import { Helpers } from "./src/types/helpers.ts";
+import { Monitor } from "./src/types/monitors.ts";
+import { Task } from "./src/types/tasks.ts";
 
 export const botCache = {
   fullyReady: false,
   dispatchedGuildIDs: new Set<string>(),
   dispatchedChannelIDs: new Set<string>(),
-  // arguments: new Collection<string, Argument>(),
-  // commands: new Collection<string, Command<any>>(),
-  // eventHandlers: {} as CustomEvents,
+  arguments: new Collection<string, Argument>(),
+  commands: new Collection<string, Command<any>>(),
+  eventHandlers: {} as CustomEvents,
 
   vipUserIDs: new Set(configs.userIDs.botOwners),
 
@@ -19,17 +26,17 @@ export const botCache = {
   guildLanguages: new Collection<string, string>(),
   vipGuildIDs: new Set([configs.supportServerID]),
 
-  // messageCollectors: new Collection<string, MessageCollector>(),
-  // reactionCollectors: new Collection<string, ReactionCollector>(),
-  // inhibitors: new Collection<string, (message: Message, command: Command<any>, guild?: Guild) => Promise<boolean>>(),
-  // monitors: new Collection<string, Monitor>(),
-  // permissionLevels: new Collection<
-  //   PermissionLevels,
-  //   (message: Message, command: Command<any>, guild?: Guild) => Promise<boolean>
-  // >(),
-  // tasks: new Collection<string, Task>(),
-  // helpers: {} as Helpers,
-  // constants: {} as Constants,
+  messageCollectors: new Collection<string, MessageCollector>(),
+  reactionCollectors: new Collection<string, ReactionCollector>(),
+  inhibitors: new Collection<string, (message: Message, command: Command<any>, guild?: Guild) => Promise<boolean>>(),
+  monitors: new Collection<string, Monitor>(),
+  permissionLevels: new Collection<
+    PermissionLevels,
+    (message: Message, command: Command<any>, guild?: Guild) => Promise<boolean>
+  >(),
+  tasks: new Collection<string, Task>(),
+  helpers: {} as Helpers,
+  constants: {} as Constants,
   stats: {
     messagesProcessed: 0,
     messagesDeleted: 0,
