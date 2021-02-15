536b36836ec44195e7afd9ad1e9bd5aef22bb514
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-15 22:31:58 +0100

    Create deps.ts

---
+++ deps.ts
@@ -0,0 +1,3 @@
+export * from "https://deno.land/std@0.87.0/fmt/colors.ts";
+export * from "https://deno.land/x/discordeno@10.3.0/mod.ts";
+export * from "https://raw.githubusercontent.com/Skillz4Killz/Sabr/master/mod.ts";
