98237c3db5b229d4aed17b28be19713bd62f8c67
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-15 22:54:22 +0100

    add getTime

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -35,3 +35,18 @@ export async function fileLoader() {
   );
   paths = [];
 }
+
+export function getTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minute = now.getMinutes();
+
+  let hour = hours;
+  let amOrPm = `AM`;
+  if (hour > 12) {
+    amOrPm = `PM`;
+    hour = hour - 12;
+  }
+
+  return `${hour >= 10 ? hour : `0${hour}`}:${minute >= 10 ? minute : `0${minute}`} ${amOrPm}`;
+}
