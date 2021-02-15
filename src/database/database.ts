802cd5098f6f47a7a2872ffd11013a252f59788d
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-15 22:52:20 +0100

    Create database.ts

---
+++ src/database/database.ts
@@ -0,0 +1,18 @@
+import { Sabr, SabrTable } from "../../deps.ts";
+import { GuildSchema } from "./schemas.ts";
+
+// Create the database class
+const sabr = new Sabr();
+// DEBUGGING CAN SHUT IT UP
+sabr.error = async function () {};
+
+export const db = {
+  // This will allow us to access table methods easily as we will see below.
+  sabr,
+  guilds: new SabrTable<GuildSchema>(sabr, "guilds"),
+};
+
+// This is important as it prepares all the tables.
+await sabr.init();
+
+console.log(`Fetching all settings that need to be cached`);
