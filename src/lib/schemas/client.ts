39cdef45ef1236aff6299f17bd54bbaa57554693
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-15 22:49:05 +0100

    delete old files

--- src/lib/schemas/client.ts
+++
@@ -1,4 +0,0 @@
-import { KlasaClient } from 'klasa';
-
-KlasaClient.defaultClientSchema
-	.add('guildTemplates', 'guild', { array: true });
--- src/lib/types/settings/ClientSettings.ts
+++
@@ -1,5 +0,0 @@
-import { Snowflake } from 'discord.js';
-export namespace ClientSettings {
-	export type GuildTemplates = Snowflake[];
-	export const GuildTemplates = 'guildTemplates';
-}
