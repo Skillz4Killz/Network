e7486a4d0e3a8b9a602a2e32fba5aeccc87d0c82
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-15 23:21:58 +0100

    add(types): helpers

--- src/types/helpers.ts
+++ src/types/helpers.ts
@@ -1,1 +1,18 @@
-export interface Helpers {}
+import { Message } from "../../deps.ts";
+
+export interface Helpers {
+  // Collectors
+  needMessage: (memberID: string, channelID: string, options?: MessageCollectorOptions | undefined) => Promise<Message>;
+  collectMessages: (options: CollectMessagesOptions) => Promise<Message[]>;
+  needReaction: (memberID: string, messageID: string, options?: ReactionCollectorOptions) => Promise<string>;
+  collectReactions: (options: CollectReactionsOptions) => Promise<string[]>;
+  processReactionCollectors: (
+    message: Message | MessageReactionUncachedPayload,
+    emoji: ReactionPayload,
+    userID: string
+  ) => void;
+
+  // Discord Helpers
+  reactError: (message: Message, vip?: boolean) => Promise<void>;
+  reactSuccess: (message: Message) => Promise<void>;
+}
