c616aba46700dff07ffd2d22b06897bf0253a748
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-16 20:39:37 +0100

    Create ...string.ts

---
+++ src/arguments/...string.ts
@@ -0,0 +1,10 @@
+import { botCache } from "../../deps.ts";
+
+botCache.arguments.set("...string", {
+  name: "...string",
+  execute: async function (argument, parameters) {
+    if (!parameters.length) return;
+
+    return argument.lowercase ? parameters.join(" ").toLowerCase() : parameters.join(" ");
+  },
+});
