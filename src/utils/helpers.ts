017ed4dbe74c874239e305e2a3ef945346ce9c97
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-16 20:27:13 +0100

    add(utils): command helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,6 @@
+import { botCache, Collection } from "../../deps.ts";
+import { ArgumentDefinition, Command } from "../types/commands.ts";
+
 // Very important to make sure files are reloaded properly
 let uniqueFilePathCounter = 0;
 let paths: string[] = [];
@@ -49,4 +52,65 @@ export function getTime() {
   }
 
   return `${hour >= 10 ? hour : `0${hour}`}:${minute >= 10 ? minute : `0${minute}`} ${amOrPm}`;
+}
+
+export function createCommand<T extends readonly ArgumentDefinition[]>(command: Command<T>) {
+  const custom = [...(command.botChannelPermissions || [])];
+
+  command.botChannelPermissions = [
+    "ADD_REACTIONS",
+    "USE_EXTERNAL_EMOJIS",
+    "READ_MESSAGE_HISTORY",
+    "VIEW_CHANNEL",
+    "SEND_MESSAGES",
+    "EMBED_LINKS",
+    ...custom,
+  ];
+
+  botCache.commands.set(command.name, command);
+}
+
+export function createSubcommand<T extends readonly ArgumentDefinition[]>(
+  commandName: string,
+  subcommand: Command<T>,
+  retries = 0
+) {
+  const names = commandName.split("-");
+
+  let command: Command<T> = botCache.commands.get(commandName)!;
+
+  if (names.length > 1) {
+    for (const name of names) {
+      const validCommand = command ? command.subcommands?.get(name) : botCache.commands.get(name);
+
+      if (!validCommand) {
+        if (retries === 20) break;
+        setTimeout(
+          () => createSubcommand(commandName, subcommand, retries++),
+          botCache.constants.milliseconds.SECOND * 10
+        );
+        return;
+      }
+
+      command = validCommand;
+    }
+  }
+
+  if (!command) {
+    // If 10 minutes have passed something must have been wrong
+    if (retries === 20) {
+      return console.log(`Subcommand ${subcommand} unable to be created for ${commandName}`);
+    }
+
+    // Try again in 10 seconds in case this command file just has not been loaded yet.
+    setTimeout(() => createSubcommand(commandName, subcommand, retries++), botCache.constants.milliseconds.SECOND * 10);
+    return;
+  }
+
+  if (!command.subcommands) {
+    command.subcommands = new Collection();
+  }
+
+  // console.log("Creating subcommand", command.name, subcommand.name);
+  command.subcommands.set(subcommand.name, subcommand);
 }
