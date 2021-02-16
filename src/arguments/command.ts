11d86d55ddaa9081b90fdf0aac3957506dadc528
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-16 20:39:46 +0100

    Create command.ts

---
+++ src/arguments/command.ts
@@ -0,0 +1,16 @@
+import { botCache } from "../../deps.ts";
+
+botCache.arguments.set("command", {
+  name: "command",
+  execute: async function (_argument, parameters) {
+    const [name] = parameters;
+    if (!name) return;
+
+    const commandName = name.toLowerCase();
+    const command = botCache.commands.get(commandName);
+    if (command) return command;
+
+    // Check if its an alias
+    return botCache.commands.find((cmd) => Boolean(cmd.aliases?.includes(commandName)));
+  },
+});
