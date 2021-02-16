03e98762ce3d1ad0a86d6749b4945887b7210e85
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-16 21:50:23 +0100

    Update utils.ts

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -56,3 +56,44 @@ botCache.helpers.cleanNumber = function (number: bigint | number | string) {
 
   return result;
 };
+
+botCache.helpers.shortNumber = function (number: bigint | number | string) {
+  const digits = number.toString();
+  // Less than 1000
+  if (digits.length < 4) return digits;
+
+  const sets: [number, number, string][] = [
+    [7, 3, "K"],
+    [10, 6, "M"],
+    [13, 9, "B"],
+    [16, 12, "T"],
+    [19, 15, "q"],
+    [22, 18, "Q"],
+    [25, 21, "s"],
+    [28, 24, "S"],
+    [32, 27, "o"],
+    [35, 30, "n"],
+    [38, 33, "d"],
+    [42, 36, "U"],
+    [45, 39, "Du"],
+    [48, 42, "Td"],
+    [52, 45, "qd"],
+    [55, 48, "Qd"],
+    [58, 51, "sd"],
+    [62, 54, "Sd"],
+    [65, 57, "Od"],
+    [68, 60, "Nd"],
+    [72, 63, "V"],
+    [75, 66, "aa"],
+    [78, 69, "ab"],
+  ];
+
+  for (const [length, size, letter] of sets) {
+    if (digits.length >= length) continue;
+    const index = digits.length - size;
+    const end = digits.slice(index, index + 2);
+    return `${digits.slice(0, index)}${end === "00" ? "" : `.${end}`}${letter}`;
+  }
+
+  return digits;
+};
